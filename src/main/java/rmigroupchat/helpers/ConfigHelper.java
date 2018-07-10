cd8300c863b1e2aa1dd16298ded5cac435f0b1b4
Yago Tomé <yago.tome@hotmail.com>
2018-07-10 13:13:30 -0300

    Create config helper

---
+++ src/main/java/rmigroupchat/helpers/ConfigHelper.java
@@ -0,0 +1,19 @@
+package rmigroupchat.helpers;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+import rmigroupchat.helpers.model.Config;
+
+public class ConfigHelper {
+	private static final String CONFIG_URI = "config.json";
+	private static Gson gson = new Gson();
+	
+	public static Config getConfig() throws JsonSyntaxException, JsonIOException, FileNotFoundException  {
+		return gson.fromJson(new FileReader(CONFIG_URI), Config.class);
+	}
+}
---
+++ src/main/java/rmigroupchat/helpers/model/Config.java
@@ -0,0 +1,13 @@
+package rmigroupchat.helpers.model;
+
+public class Config {
+	private String rmiRegistryHost;
+
+	public String getRmiRegistryHost() {
+		return rmiRegistryHost;
+	}
+
+	public void setRmiRegistryHost(String rmiRegistryHost) {
+		this.rmiRegistryHost = rmiRegistryHost;
+	}
+}
