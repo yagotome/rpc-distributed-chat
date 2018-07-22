85232ce5511712753dd25bc0a247e9ff44d04126
Yago Tomé <yago.tome@hotmail.com>
2018-07-21 22:58:50 -0300

    Finish integration between server and client of Messages

--- src/main/java/rmigroupchat/rmi/MessageServer.java
+++ src/main/java/rmigroupchat/rmi/MessageServer.java
@@ -1,8 +1,16 @@
 package rmigroupchat.rmi;
 
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import rmigroupchat.helpers.RmiMultiClientHelper;
 import rmigroupchat.helpers.RmiNamingUtils;
+import rmigroupchat.model.Message;
 import rmigroupchat.socket.SocketServer;
 
 /**
@@ -17,15 +25,31 @@ public class MessageServer {
 	public static void main(String args[]) {
 		try {
 			socketServer = new SocketServer(message -> {
-				// TODO: Send message to all registered users in rmiregistry
+				try {
+					sendMessageToClients(message);
+				} catch (RemoteException | MalformedURLException | FileNotFoundException e) {
+					System.out.println("Não possível enviar a mensagem: `" + message.getFormattedMessage() + "`");
+					System.err.println("Error: " + e.getMessage());
+					e.printStackTrace();
+				}
 			});
 			socketServer.start();
-			MessageService messageService = new MessageServiceImpl();
+			MessageService messageService = new MessageServiceServerImpl();
 			Naming.rebind(RmiNamingUtils.getMessageServiceName(), messageService);
 			System.out.println("MessageService bound into rmiregistry");
 		} catch (Exception ex) {
 			System.err.println("Error: " + ex.getMessage());
 			ex.printStackTrace();
 		}
+	}
+
+	private static void sendMessageToClients(Message message)
+			throws RemoteException, MalformedURLException, FileNotFoundException {
+		List<String> userRegistries = Arrays.asList(Naming.list(RmiNamingUtils.getMessageServiceName()));
+		String serverName = RmiNamingUtils.getMessageServiceName();
+		userRegistries = userRegistries.parallelStream().filter(registry -> !registry.equals(serverName))
+				.collect(Collectors.toList());
+		RmiMultiClientHelper msgMultiClient = new RmiMultiClientHelper(userRegistries);
+		msgMultiClient.send(message);
 	}
 }
--- src/main/java/rmigroupchat/ui/ChatClient.java
+++ src/main/java/rmigroupchat/ui/ChatClient.java
@@ -1,55 +1,66 @@
 package rmigroupchat.ui;
 
 import java.io.IOException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
+
+import rmigroupchat.helpers.RmiClientHelper;
+import rmigroupchat.helpers.RmiNamingUtils;
 import rmigroupchat.model.Message;
-import rmigroupchat.rmi.MessageClient;
-import rmigroupchat.socket.SocketClient;
-import rmigroupchat.socket.SocketServer;
+import rmigroupchat.rmi.MessageService;
+import rmigroupchat.rmi.MessageServiceClientImpl;
 
 public class ChatClient {
-	private static final String WELCOMING_MESSAGE = "Olá! Bem vindo ao groupchat!\nDigite seu username para entrar no chat:";
+	private static final String WELCOMING_MESSAGE = "Olá! Bem vindo ao groupchat!\nDigite seu username para entrar no chat: ";
 	private static final String USER_JOIN_MESSAGE = "*** %s entrou na conversa ***";
 	private static final String USER_LEAVE_MESSAGE = "*** %s saiu da conversa ***";
 	private static final String SENDING_ERROR_MESSAGE = "*** Erro ao tentar enviar mensagem ***";
+
 	public static void print(String text) {
 		System.out.print(text);
 	}
+
 	public static void println(String text) {
 		System.out.println(text);
-	}	
+	}
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);		
+		Scanner scanner = new Scanner(System.in);
 		try {
 			startInteractiveUI(scanner);
 		} catch (IOException | NotBoundException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Ocorreu um erro inesperado e a aplicação está sendo encerrada. Erro: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
+
 	private static void startInteractiveUI(Scanner scanner) throws IOException, NotBoundException {
 		String messageText = null;
 
 		print(WELCOMING_MESSAGE);
-		
-		
+
 		String username = scanner.nextLine();
-		// TODO: join above user to multicast group and start to listen RMI to print other user messages
-		
-		MessageClient msgClient = new MessageClient();
+		MessageService messageService = new MessageServiceClientImpl();
+		String userRmiRegistry = RmiNamingUtils.getMessageServiceName(username);
+		Naming.rebind(userRmiRegistry, messageService);
+
+		RmiClientHelper msgClient = new RmiClientHelper();
 		msgClient.send(new Message(null, String.format(USER_JOIN_MESSAGE, username)));
-		
-		while (messageText != "/exit") {
-			messageText = scanner.nextLine();
+
+		while (!(messageText = scanner.nextLine()).equals("/exit")) {
 			Message message = new Message(username, messageText);
-			try { 
+			try {
 				msgClient.send(message);
 			} catch (IOException e) {
 				println(SENDING_ERROR_MESSAGE);
 			}
 		}
 
-		msgClient.send(new Message(null, String.format(USER_LEAVE_MESSAGE, username)));		
+		Naming.unbind(userRmiRegistry);
+		UnicastRemoteObject.unexportObject(messageService, true);
+
+		msgClient.send(new Message(null, String.format(USER_LEAVE_MESSAGE, username)));
 	}
 }
